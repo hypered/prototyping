d48e275e8d417f08b7188dc1eaf7c3af4427fc47
Vo Minh Thu <thu@hypered.io>
2021-04-15 23:29:47 +0200

    Cosmetics.

--- sql/insert-screens.sql
+++ sql/insert-screens.sql
@@ -2,10 +2,11 @@
 -- prototype_metadata table. This is used in `db.Makefile`.
 
 INSERT INTO prototype_screens
-  SELECT replace(substr(source, 9),".md","") AS name,
-         json_extract(data, "$.route")       AS route,
-         json_extract(data, "$.screen")      AS type,
-         replace(replace(json_extract(data, "$.query"),"‘","'"),"’","'") as query,
-         json_extract(data, "$.ids")         AS ids
+  SELECT
+    replace(substr(source, 9),".md","")                             AS name,
+    json_extract(data, "$.route")                                   AS route,
+    json_extract(data, "$.screen")                                  AS type,
+    replace(replace(json_extract(data, "$.query"),"‘","'"),"’","'") AS query,
+    json_extract(data, "$.ids")                                     AS ids
   FROM prototype_metadata
   WHERE json_extract(data, "$.screen") IS NOT NULL
